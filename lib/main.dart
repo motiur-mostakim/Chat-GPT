fd7c4acf42a303ae814e10da22b4cd54475a2d15
Motiur_dev <motiur.togumogu@gmail.com>
2024-10-18 17:45:56 +0600

    gimini Chat app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:space_pod/pages/home_page.dart';
+
+void main() => runApp(const MyApp(),);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: Colors.grey.shade900,
+          primaryColor: Colors.grey.shade900),
+    );
+  }
+}
